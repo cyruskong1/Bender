f4b2616e88422d465ff2532ab76be761da3990fe
Cy Kong <cykong@Regie-Kongs-MacBook-Pro.local>
2017-05-22 19:32:51 -0700

    nothing

--- server/db/database.js
+++ server/db/database.js
@@ -1,4 +1,31 @@
- var db = require('mongoose');
- db.connect('mongodb://localhost/bender');
+ // var db = require('mongoose');
+ // db.connect('mongodb://localhost/bender');
+ //lets require/import the mongodb native drivers.
+ //module.exports = db;
 
- module.exports = db;
+
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+
+//(Focus on This Variable)
+var url = 'mongodb://cy.kong:perfect1@heroku_6vq8sw9s.mlab.com:1316/bender';;      
+//(Focus on This Variable)
+
+// Use connect method to connect to the Server
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+
